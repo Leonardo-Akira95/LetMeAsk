b2b7ad526b97767926efde0a9f991ee46202ecb4
Leonardo Akira Utani <leonardo.utani@fatec.sp.gov.br>
2021-12-14 15:34:01 -0300

    Criação das telas de home e criação de nova sala. Implementada a autenticação com GoogleAuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+
 function App() {
-  return (
-    <h1>Let me ask</h1>
+  return ( 
+    <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+
+    </Router>
   );
 }
 
